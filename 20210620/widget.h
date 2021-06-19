8e0badd8e6a38e9a6c5e8928877347887ed6d998
xuronghua2001 <xuronghua2001@163.com>
2021-06-19 15:23:20 +0800

    Update widget.h

--- 20210620/widget.h
+++ 20210620/widget.h
@@ -1,15 +1,11 @@
 #ifndef WIDGET_H
 #define WIDGET_H
-#include <QFileSystemModel>
 #include <QWidget>
-#include <QVBoxLayout>
 #include <QLineEdit>
 #include <QListView>
 #include <QDir>
 #include <QStringListModel>
-#include <QSortFilterProxyModel>
 #include <QMessageBox>
-#include<QListWidget>
 #include<QPaintEvent>
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
@@ -27,10 +23,9 @@ public:
 
 private slots:
     void on_pushButton_clicked();
+
     void on_listView_doubleClicked(QModelIndex index);
- //   void onDoubleClick_listView(const QModelIndex& index);
- //   void selectChanged_listView(const QItemSelection& selection);
-  //  void on_listWidget_itemDoubleClicked(QListWidgetItem *item);
+
     void on_pushButton_4_clicked();
 
     void on_pushButton_2_clicked();
@@ -46,21 +41,7 @@ private slots:
     void on_pushButton_7_clicked();
 
 private:
-    QVBoxLayout* mainLayout;
-    QStringListModel* m_string_list_model = new QStringListModel();
-    QFileSystemModel *fm;
     QStringListModel *model;
-    static QFileInfoList fileInfoList;
-    QString curFileName;
-
-
-
-
-  /*  QString m_dir_str;
-    QLineEdit* m_input_edit;
-    QListView* m_file_list_view;
-    QStringListModel* m_string_list_model;
-    QSortFilterProxyModel* m_proxy_model;*/
     Ui::Widget *ui;
 };
 #endif // WIDGET_H
